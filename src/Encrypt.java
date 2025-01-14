63e6fda6f73a14e263b4ccd863a97103b3f7ea1e
Jannick <j.huettel@hgs-singen.online>
2025-01-14 18:23:19 +0000

    Verschlüsseln der Datei und Überschreiben der Originaldatei

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 /**
  *
  * @author Jannick
@@ -12,38 +14,58 @@ public class Encrypt {
         //Eingabe (Initialisierung)
         System.out.printf("Hallo. Bitte geben sie ihre Datei ein (Dateiname und Endung):\n");
         Scanner s = new Scanner(System.in);
-        String filepath = s.next();
+        String filepath = s.nextLine();
         s.close();
-        File datei = new File("src/test.txt");
+        File datei = new File(filepath);
 
         //Verarbeitung
 
         //Einlesen der Datei
-        try{
-            byte[] inhaltUnverschlüsselt = Read(datei, filepath);
-            System.out.println(inhaltUnverschlüsselt);
-        }
-        catch(IOException e){
-            System.out.printf("Die Datei %s koonte nicht eingelesen werden. Bitte überprüfen sie ihre Eingabe.\nBeachten sie, dass sie den kompletten absoluten oder relativen Pfad eingeben müssen.", filepath);
-        }
-
+        
+        byte[] inhaltUnverschlüsselt = ReadFile(datei, filepath);
+        System.out.println(inhaltUnverschlüsselt);
+        
         //Verschlüsseln der Datei
-        //Invert();
+        byte[] inhaltVerschlüsselt = Invert(inhaltUnverschlüsselt);
         
         //(Über-) Schreiben der Datei
+        WriteFile(inhaltVerschlüsselt, filepath);
                 
     }
             
             
-            public static byte[] Read(File datei, String filepath) throws IOException{
-                byte[] inhaltUnverschlüsselt = Files.readAllBytes(datei.toPath());
-                System.out.println("Datei erfolgreich eingelesen.");
-                return inhaltUnverschlüsselt;
-            }
+    public static byte[] ReadFile(File datei, String filepath){
+        try{
+            byte[] inhaltUnverschlüsselt = Files.readAllBytes(datei.toPath());
+            System.out.println("Datei erfolgreich eingelesen.");
+            return inhaltUnverschlüsselt;
+        }
+        catch(IOException e){
+            System.out.printf("Die Datei %s koonte nicht eingelesen werden. Bitte überprüfen sie ihre Eingabe.\nBeachten sie, dass sie den kompletten absoluten oder relativen Pfad eingeben müssen.", filepath);
+            return null;
+        }
+    }
 
-            /*public static void Invert(byte[] inhaltUnverschlüsselt) {
-                for (int i; )
-            }*/
+    public static byte[] Invert(byte[] inhaltUnverschlüsselt) {
+        byte[] inhaltVerschlüsselt = new byte[inhaltUnverschlüsselt.length];
+        for (int i = 0;i < inhaltUnverschlüsselt.length;i++ ){
+            inhaltVerschlüsselt[i] = (byte) (inhaltUnverschlüsselt[i]^ 0xff);
+        }
+        System.out.println("Datei erfogreich verschlüsselt.");
+        System.out.println(inhaltVerschlüsselt);
+        return inhaltVerschlüsselt;
+    }
+
+    public static void WriteFile(byte[] inhaltUnverschlüsselt, String filepath){
+        try{
+            Path path = Paths.get(filepath);
+            Files.write(path,inhaltUnverschlüsselt);
+            System.out.println("Datei erfolgreich überschrieben.");
+        }
+        catch(IOException e) {
+            System.out.printf("Die Datei %s konnte nicht überschrieben werden.", filepath);
+        }
+    }
 
 
 }
